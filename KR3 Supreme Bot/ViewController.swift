8afb59b2304842c0401f706674c4112715853fa7
Дмитрий Антоник <KR3ND31@mac-mini-dmitriy.local>
2018-04-01 17:33:24 +0300

    Initial Commit

---
+++ KR3 Supreme Bot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KR3 Supreme Bot
+//
+//  Created by Дмитрий Антоник on 01.04.2018.
+//  Copyright © 2018 KR3ND31. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ KR3 Supreme BotTests/KR3_Supreme_BotTests.swift
@@ -0,0 +1,36 @@
+//
+//  KR3_Supreme_BotTests.swift
+//  KR3 Supreme BotTests
+//
+//  Created by Дмитрий Антоник on 01.04.2018.
+//  Copyright © 2018 KR3ND31. All rights reserved.
+//
+
+import XCTest
+@testable import KR3_Supreme_Bot
+
+class KR3_Supreme_BotTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
