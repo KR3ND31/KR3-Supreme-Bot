e07430e22bae84883fad16a9b19869381db21ba0
Дмитрий Антоник <KR3ND31@mac-mini-dmitriy.local>
2018-04-15 11:54:04 +0300

    1

---
+++ Pods/Target Support Files/Pods-KR3 Supreme BotTests/Pods-KR3 Supreme BotTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KR3_Supreme_BotTests : NSObject
+@end
+@implementation PodsDummy_Pods_KR3_Supreme_BotTests
+@end
---
+++ Pods/Target Support Files/Pods-KR3 Supreme BotUITests/Pods-KR3 Supreme BotUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KR3_Supreme_BotUITests : NSObject
+@end
+@implementation PodsDummy_Pods_KR3_Supreme_BotUITests
+@end
